acd6e51b0b5a74c73a4bb8c1be3d0ddb5f9eb657
PabloRomeroAbadie <pabloabadie1995@gmail.com>
2024-10-25 17:42:11 -0300

    fix bugs

--- main.ts
+++ main.ts
@@ -10,6 +10,15 @@ enum Cell {
 
 type Direction = "down" | "left" | "right";
 
+const config = {
+  DROP_INTERVAL: 1000,
+  POINTS: 0,
+  LEVEL: 1,
+  LINES_CLEARED: 0,
+  LINES_TO_NEXT_LEVEL: 10
+}
+
+
 class Tetris {
   private readonly rowsCount: number = 20; //cantidad de filas
   private readonly columnsCount: number = 10; //cantidad de columnas
@@ -19,6 +28,14 @@ class Tetris {
   private tetriminos: Tetrimino[] = [];
   private currentTetrimino: Tetrimino | undefined;
   private forceLoop: boolean = false;
+  private points: number = config.POINTS;
+  private dropInterval = config.DROP_INTERVAL; // Intervalo inicial en milisegundos
+  private level = config.LEVEL;
+  private linesCleared = config.LINES_CLEARED;
+  private linesToNextLevel = config.LINES_TO_NEXT_LEVEL
+  private listenKeys = false
+  private stopGame = false
+  private resetGame = false
 
   constructor() {}
 
@@ -33,16 +50,71 @@ class Tetris {
     Aqui va toda la lógica del juego dentro del loop
   */
   private mainLoop(){
-    
-    this.clearTable()
 
     this.lowerTetrimino()
 
     if(!this.currentTetrimino){
       this.currentTetrimino = this.getRandomTetrimino()
+      this.currentTetrimino.posY += 1
+      if(this.isCollision()){
+        this.stateLoop = false
+      }
+      this.currentTetrimino.posY -=1
     }
-    // console.log("main loop")
-    this.drawTable(); //dibujo la tabla
+
+    this.updateTableWithTetrimino(this.currentTetrimino, false)
+    this.verifyAndDeleteLine();
+  }
+
+  /*
+    Función que se encarga de verificar si es que hay linea, si las hay que las destruya
+  */
+  private verifyAndDeleteLine(){
+    const indexRowsToDelete = []
+    for (let i = 0; i < this.rowsCount; i++) { // Recorro las filas
+      if (!this.table[i]) {
+          this.table[i] = [];
+      }
+      let lineComplete = true
+      for (let j = 0; j < this.columnsCount; j++) { // Recorro las columnas
+        if(this.table[i][j] == Cell.empty || this.table[i][j] == Cell.not_fixed){
+          lineComplete = false
+        }
+      }
+
+      if(lineComplete){
+        indexRowsToDelete.push(i)
+      }
+    }
+
+    for (let i = 0; i < indexRowsToDelete.length; i++) {
+      const index = indexRowsToDelete[i];
+      this.table.splice(index, 1) 
+      this.table.unshift(new Array(10).fill(1))   
+    }
+    this.linesCleared += indexRowsToDelete.length  
+
+    if(this.linesCleared > this.linesToNextLevel){
+      this.linesCleared -= this.linesToNextLevel
+      this.level++
+      this.dropInterval = Math.max(100, this.dropInterval - 100)
+      if(this.dropInterval < 0.2){
+        this.dropInterval = 0.2
+      }
+    }
+
+    if(indexRowsToDelete.length == 1){
+      this.points += 40 * (this.level * 0.1)
+    }
+    if(indexRowsToDelete.length == 2){
+      this.points += 100 * (this.level * 0.1)
+    }
+    if(indexRowsToDelete.length == 3){
+      this.points += 300 * (this.level * 0.1)
+    }
+    if(indexRowsToDelete.length == 4){
+      this.points += 1200 * (this.level * 0.1)
+    }
   }
 
   /*
@@ -53,7 +125,7 @@ class Tetris {
     Deno.stdin.setRaw(true);
     
     try {
-      while (this.stateLoop) {
+      while ((this.stateLoop || this.listenKeys) && !this.stopGame) {
         const bytesRead = await Deno.stdin.read(buffer);
         if (bytesRead !== null) {
           const key = new TextDecoder().decode(buffer.slice(0, bytesRead));
@@ -63,23 +135,32 @@ class Tetris {
             case "\x1b[A": // Flecha arriba
                 this.currentTetrimino?.rotate()
                 if(this.isCollision()) this.currentTetrimino?.reverse()
-                this.clearTable()
-                this.drawTable()
+                this.updateTableWithTetrimino(this.currentTetrimino as Tetrimino, false)
                 break;
             case "\x1b[B": // Flecha abajo
                 this.moveTetriminio('down')
+                this.updateTableWithTetrimino(this.currentTetrimino as Tetrimino, false)
                 break;
             case "\x1b[C": // Flecha derecha
                 this.moveTetriminio('right')
+                this.updateTableWithTetrimino(this.currentTetrimino as Tetrimino, false)
                 break;
             case "\x1b[D": // Flecha izquierda
                 this.moveTetriminio('left')
+                this.updateTableWithTetrimino(this.currentTetrimino as Tetrimino, false)
                 break;
             case "z":
-                this.stateLoop = false; // Cambiar estado para salir del bucle principal
+                this.stateLoop = false
+                this.stopGame = true
+                break;
+            case "r":
+                console.log("reiniciando...")
+                this.stateLoop = false
+                this.stopGame = true
+                this.resetGame = true
                 break;
             default:
-                console.log(`Tecla "${key}" detectada.`);
+                console.log(``);
                 break;
           }
         }
@@ -87,6 +168,20 @@ class Tetris {
     } finally {
         Deno.stdin.setRaw(false); // Restaurar el modo de consola cuando termine
     }
+  }
+
+  private reset(){
+    this.points = config.POINTS;
+    this.dropInterval = config.DROP_INTERVAL; // Intervalo inicial en milisegundos
+    this.level = config.LEVEL;
+    this.linesCleared = config.LINES_CLEARED;
+    this.linesToNextLevel = config.LINES_TO_NEXT_LEVEL
+    this.currentTetrimino = undefined
+    this.fillTable(Cell.empty)
+    this.listenKeys = false
+    this.resetGame = false
+    this.stopGame = false
+    this.loop()
   }
   
   
@@ -99,10 +194,9 @@ class Tetris {
   
       while (this.stateLoop) { // Mientras no sea gameOver que loopee
           const currentTime = new Date().getTime() - time; // Obtengo cuánto tiempo pasó
-          if (currentTime > 1000 || this.forceLoop) { // Si pasó más del tiempo estimado entonces ejecuto
-              time = new Date().getTime(); // Reseteo el tiempo
-              if(this.forceLoop){
-                // console.log("por event loop...")
+          if (currentTime > this.dropInterval || this.forceLoop) { // Si pasó más del tiempo estimado entonces ejecuto
+              if(!this.forceLoop){
+                time = new Date().getTime(); // Reseteo el tiempo
               }
               this.forceLoop = false;
   
@@ -113,27 +207,14 @@ class Tetris {
           await new Promise(resolve => setTimeout(resolve, 0));
       }
   
-      console.log("Saliendo del bucle principal.");
-  }  
+      console.log("Game Over");
+      this.listenKeys = true
+      this.listenForKeyPresses()
 
-  /*
-    Aqui limpia las celdas de la tabla
-    @param notFixed: boolean -> Para saber si ignoro las celdas fijas o no
-  */
-  private clearTable(notFixed = false){
-    this.iterateTable((value: number) => {
-      if(notFixed){
-        return Cell.empty
+      if(this.resetGame){
+        this.reset()
       }
-      if(value == Cell.empty){
-        return Cell.empty
-      }
-      if(value == Cell.fixed){
-        return Cell.fixed
-      }
-      return Cell.empty
-    },true)
-  }
+  }  
 
   /*
     Mueve para abajo al tetrimino
@@ -144,15 +225,16 @@ class Tetris {
 
     this.currentTetrimino.posY += 1
     if(this.isCollision()){
+      if(this.currentTetrimino.posY <= 0){
+        this.stateLoop = false
+      }
       this.currentTetrimino.posY -= 1
-      this.clearTable()
-      this.setFixTetrimino(this.currentTetrimino, 0.5)
+      this.updateTableWithTetrimino(this.currentTetrimino, true)
       this.currentTetrimino = undefined
-      this.drawTable()
     }
 
     if(this.currentTetrimino){
-      this.setNotFixedTetrimino(this.currentTetrimino)
+      this.updateTableWithTetrimino(this.currentTetrimino, false)
     }
     
   }
@@ -186,34 +268,9 @@ class Tetris {
     }
 
     if(this.currentTetrimino){
-      this.clearTable()
-      this.setNotFixedTetrimino(this.currentTetrimino)
-      this.drawTable()
+      this.updateTableWithTetrimino(this.currentTetrimino, false)
     }
     
-  }
-
-  /*
-    Coloca en la tabla las celdas fijadas del tetrimino en juego
-  */
-  private setFixTetrimino(_T: Tetrimino, seconds: number){
-    
-    this.iterateTetriminoMap(_T, (tableValue, TValue) => {
-      if(tableValue == Cell.fixed) return Cell.fixed
-      return TValue === 1 ? Cell.fixed : Cell.empty;
-    }, true)
-
-  }
-  
-  /*
-    Coloca en la tabla las celdas no fijadas del tetrimino en juego
-  */
-  private setNotFixedTetrimino(_T: Tetrimino){
-    this.iterateTetriminoMap(_T, (tableValue, TValue) => {
-      if(tableValue == Cell.fixed) return Cell.fixed
-      else if(TValue == 1) return Cell.not_fixed
-      else return Cell.empty
-    }, true)
   }
 
   /*
@@ -224,6 +281,8 @@ class Tetris {
     let collision: boolean = false;
 
     if(!this.currentTetrimino) return collision
+
+    if(this.currentTetrimino.posY < 0) return collision
 
     this.iterateTetriminoMap(this.currentTetrimino, (tableValue, tValue) => {
       //si la posición hace que salga de la tabla
@@ -237,48 +296,72 @@ class Tetris {
     })
 
     return collision;
-  }  
+  }
 
-  /*
-    Función que dibuja la tabla
-    valores posibles de la celda
-    0 = espacio vacio
-    1 = bloque no fijado
-    2 = bloque fijado
-  */
-  private drawTable() {
-    let text = '' //variable para guardar el output a la consola
-    for (let i = 0; i < this.rowsCount; i++) { //recorro las filas
-      for (let j = 0; j < this.columnsCount; j++) { //recorro las columnas
+  // Función que dibuja la tabla
+  //   valores posibles de la celda
+  //   0 = espacio vacio
+  //   1 = bloque no fijado
+  //   2 = bloque fijado
+  private updateTableWithTetrimino(_T: Tetrimino, fixed: boolean) {
+    try {
+      let text = ''; // Variable para el output en la consola
 
-        if (j == 0) { //si es el primero agrego estructura visual
-          text += '<'
-          text += '!'
-        }
+      for (let i = 0; i < this.rowsCount; i++) { // Recorro las filas
+          if (!this.table[i]) {
+              this.table[i] = [];
+          }
 
-        if(this.table[i][j] == Cell.empty){
-          text += "--"
-        }
-        if(this.table[i][j] == Cell.fixed || this.table[i][j] == Cell.not_fixed){
-          text += "[]"
-        }
+          for (let j = 0; j < this.columnsCount; j++) { // Recorro las columnas
 
-        if (j == (this.columnsCount - 1)) { //si es el último agrego estructura visual
-          text += '!'
-          text += '>'
-        }
+              // 1. Actualizar valores existentes en la tabla
+              if (this.table[i][j] === 2) {
+                  // Si el valor actual es 2 (no fijado), se debe limpiar a 1 (vacío)
+                  this.table[i][j] = 1;
+              }
+
+              // 2. Colocar el Tetrimino basado en la posición del _T
+              const TValue = _T.map[i - _T.posY] && _T.map[i - _T.posY][j - _T.posX];
+              if (TValue === 1) {
+                  // Si el Tetrimino tiene una parte activa aquí, establecerla como 2 o 3
+                  this.table[i][j] = fixed ? 3 : 2;
+              }
+
+              // 3. Generar el texto para la visualización en consola
+              if (j === 0) { // Si es el primero agrego estructura visual
+                  text += '<';
+                  text += '!';
+              }
+
+              // Añadir contenido según el estado de la celda
+              if (this.table[i][j] === 1) {
+                  text += '--'; // Vacío
+              } else if (this.table[i][j] === 2 || this.table[i][j] === 3) {
+                  text += '[]'; // Tetrimino no fijado o fijado
+              }
+
+              if (j === (this.columnsCount - 1)) { // Si es el último agrego estructura visual
+                  text += '!';
+                  text += '>';
+              }
+          }
+          text += '\n';
       }
-      text += '\n'
-    }
 
-    //antes de finalizar agrego estructura visual de abajo
-    text += '<!'
-    for (let i = 0; i < this.columnsCount; i++) {
-      text += '=='
-    }
-    text += '!>'
+      // Estructura visual de la parte inferior
+      text += '<!';
+      for (let i = 0; i < this.columnsCount; i++) {
+          text += '==';
+      }
+      text += '!>';
+      text += '\n';
+      text += 'Points: '+this.points;
 
-    this.printConsole(text) //dibujo en consola
+      // Imprimir en la consola
+      this.printConsole(text);
+    } catch (error) {
+      // console.log("error:  ",error)
+    }
   }
 
   /*
